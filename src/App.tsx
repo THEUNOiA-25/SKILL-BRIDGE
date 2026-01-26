1b2de7ab3d1a56cfebab851676910ba9800f4322
THEUNOiA-25 <omchithaswaaroop@theunoia.com>
2026-01-26 23:26:52 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// trigger vercel rebuild
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
